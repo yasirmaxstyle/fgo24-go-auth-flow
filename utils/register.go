1571a666caedd7b6d0a1f4f4e5e81a9008762b74
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-10 17:00:05 +0700

    feat: enable multiple user account registration

--- utils/register.go
+++ utils/register.go
@@ -1,18 +1,35 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type user struct {
 	username string
 	password string
 }
 
-var dataUser user
+var dataUser []user
+
+var newReg user
+
+func clear() {
+	fmt.Print("\033[H\033[2J")
+}
 
 func register() {
+	fmt.Println("-----SIGN UP-----")
 	fmt.Print("Enter your name: ")
-	fmt.Scanln(&dataUser.username)
+	fmt.Scanln(&newReg.username)
 
-  fmt.Print("Enter your password: ")
-	fmt.Scanln(&dataUser.password)
+	fmt.Print("Enter your password: ")
+	fmt.Scanln(&newReg.password)
+	newReg.password = hashPassword(newReg.password)
+
+	dataUser = append(dataUser, newReg)
+	clear()
+
+	redirecting("Registartion is successful, redirecting to login page...")
+
+	login()
 }
