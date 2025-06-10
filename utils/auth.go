3ad92651fda7812597f8ea920d0dfa9f892cdcc8
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-10 16:57:01 +0700

    feat: add auth home interface

--- utils/auth.go
+++ utils/auth.go
@@ -1,6 +1,28 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func redirecting(msg string) {
+	fmt.Print(msg)
+	time.Sleep(time.Second * 1)
+	clear()
+}
+
 func Auth() {
-	register()
-	login()
+	fmt.Println("-----WELCOME-----")
+	fmt.Println("Go to register if you are new or login if you have registered?\n1. Register\n2. Login")
+	fmt.Scanln(&choice)
+	if choice == 1 {
+		clear()
+		register()
+	} else if choice == 2 {
+		clear()
+		login()
+	} else {
+		os.Exit(1)
+	}
 }
